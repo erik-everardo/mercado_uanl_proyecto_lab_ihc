60a4380a063c0edd310ab2d64c79d6761ee66a63
erik <erik10cavazos@gmail.com>
2019-10-27 16:19:05 -0600

    creacion paginas

---
+++ Pages/Vender.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace mercado_uanl.Pages
+{
+    public class Vender : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
