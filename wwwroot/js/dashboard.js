f7f16e1a0f1e997280b42b78cdcbd6209011011c
erik <erik10cavazos@gmail.com>
2019-10-28 00:35:31 -0600

    diversos cambios

---
+++ wwwroot/js/dashboard.js
@@ -0,0 +1,72 @@
+//referencias
+var pantallaPrincipalDiv = document.getElementById("principal");
+var pantallaVentasDiv = document.getElementById("vender");
+var pantallaCalificacionesDiv = document.getElementById("calificaciones");
+var linkPantallaPrincipal = document.getElementById("btn-principal");
+var linkPantallaVentas = document.getElementById("btn-vender");
+var linkPantallaCalificaciones = document.getElementById("btn-calificaciones");
+var sidebar = document.getElementById("sidebar");
+var main = document.getElementById("main-dashboard");
+
+$('#btn-principal').on('click',function () {
+    //aparecer capa principal y desaparecer las otras
+    pantallaPrincipalDiv.style.display = "block";
+    pantallaCalificacionesDiv.style.display = "none";
+    pantallaVentasDiv.style.display = "none";
+    
+    //colocar el link como active
+    if(!linkPantallaPrincipal.classList.contains("active")){
+        linkPantallaPrincipal.classList.add("active");
+    }
+    if(linkPantallaCalificaciones.classList.contains("active")){
+        linkPantallaCalificaciones.classList.remove("active");
+    }
+    if(linkPantallaVentas.classList.contains("active")){
+        linkPantallaVentas.classList.remove("active");
+    }
+});
+$('#btn-vender').on('click',function () {
+   //aparecer capa ventas y desaparecer las otras
+   pantallaVentasDiv.style.display = "block";
+   pantallaCalificacionesDiv.style.display = "none";
+   pantallaPrincipalDiv.style.display = "none";
+
+    //colocar el link como active
+    if(!linkPantallaVentas.classList.contains("active")){
+        linkPantallaVentas.classList.add("active");
+    }
+    if(linkPantallaCalificaciones.classList.contains("active")){
+        linkPantallaCalificaciones.classList.remove("active");
+    }
+    if(linkPantallaPrincipal.classList.contains("active")){
+        linkPantallaPrincipal.classList.remove("active");
+    }
+    ActualizarMisProductos();
+});
+$('#btn-calificaciones').on('click',function () {
+   //aparecer capa calificaciones y desaparecer las otras
+   pantallaPrincipalDiv.style.display = "none";
+   pantallaCalificacionesDiv.style.display = "block";
+   pantallaVentasDiv.style.display = "none";
+
+    //colocar el link como active
+    if(!linkPantallaCalificaciones.classList.contains("active")){
+        linkPantallaCalificaciones.classList.add("active");
+    }
+    if(linkPantallaPrincipal.classList.contains("active")){
+        linkPantallaPrincipal.classList.remove("active");
+    }
+    if(linkPantallaVentas.classList.contains("active")){
+        linkPantallaVentas.classList.remove("active");
+    }
+});
+
+function desplegarSidebar(){
+    if(sidebar.style.display === "block"){
+        sidebar.style.display = "none";
+        main.style.marginLeft = "0";
+    }else{
+        sidebar.style.display = "block";
+        main.style.marginLeft = "auto";
+    }
+}
