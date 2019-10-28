7dd433d4039679f34719b4213d329c4b4df7a0bf
erik <erik10cavazos@gmail.com>
2019-10-28 00:36:31 -0600

    creadas y funcionando

--- Pages/Vender.cshtml.cs
+++ Pages/Vender.cshtml.cs
@@ -1,12 +1,40 @@
+using mercado_uanl.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace mercado_uanl.Pages
 {
     public class Vender : PageModel
     {
-        public void OnGet()
+        private readonly DbContextApp contexto;
+
+        public Vender(DbContextApp contexto)
         {
+            this.contexto = contexto;
+        }
+
+        public bool ExistioError = false;
+        public IActionResult OnPost(string id,string pass,string Nombre,string Descripcion,string Categoria, string precio, string urlImagen="")
+        {
+            var ProductoNuevo = new Producto();
+            ProductoNuevo.Nombre = Nombre;
+            ProductoNuevo.Descripcion = Descripcion;
+            ProductoNuevo.Categoria = Categoria;
+            ProductoNuevo.Precio = float.Parse(precio);
+            ProductoNuevo.UrlFoto = urlImagen;
+            ProductoNuevo.IdUsuario = int.Parse(id);
+
+            if (contexto.Usuarios.Find(int.Parse(id)).Password.Equals(pass))
+            {
+                contexto.Productos.Add(ProductoNuevo);
+                contexto.SaveChanges();
+            }
+            else
+            {
+                ExistioError = true;
+            }
             
+            return Page();
         }
     }
 }
