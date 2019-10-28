5b485f9450a51680a7b452698e23a0e03893e789
erik <erik10cavazos@gmail.com>
2019-10-28 00:36:27 -0600

    creadas y funcionando

--- Pages/Vendiendo.cshtml.cs
+++ Pages/Vendiendo.cshtml.cs
@@ -1,12 +1,30 @@
+using System.Collections.Generic;
+using System.Linq;
+using mercado_uanl.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace mercado_uanl.Pages
 {
     public class Vendiendo : PageModel
     {
-        public void OnGet()
+        private readonly DbContextApp contexto;
+        public List<Producto> ProductosDelUsuario;
+        public Vendiendo(DbContextApp contexto)
         {
-            
+            this.contexto = contexto;
+        }
+
+        public IActionResult OnPost(string usuario, string password)
+        {
+            //verificar usuario y password
+            if (contexto.Usuarios.Find(int.Parse(usuario)).Password.Equals(password))
+            {
+                ProductosDelUsuario =
+                    contexto.Productos.Where(producto => producto.IdUsuario.Equals(int.Parse(usuario))).ToList();
+            }
+
+            return Page();
         }
     }
 }
