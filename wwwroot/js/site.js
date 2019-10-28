10e4cae0dcda552242d7d3a1efc9e8051a78e199
erik <erik10cavazos@gmail.com>
2019-10-28 00:36:52 -0600

    ajax para publicar y actualizar productos (venta)

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,6 +3,7 @@
 //referencias
 var campoPassword = document.getElementById("campo_pass");
 var campoPasswordConfirmacion = document.getElementById("campo_pass_comprobacion");
+var cardColumnsMisProductos = document.getElementById("mis_productos");
 
 $('#campo_pass_comprobacion').on('keyup',function () {
     if(campoPassword.value !== campoPasswordConfirmacion.value){
@@ -20,4 +21,26 @@ $('#campo_pass_comprobacion').on('keyup',function () {
             campoPasswordConfirmacion.classList.remove("is-invalid");
         }
     }
-})
+});
+
+$('#boton_publicar_producto_nuevo').on('click',function () {
+    var DatosAEnviar = $('#formulario_publicar_producto').serialize();
+    $.post("/Vender",DatosAEnviar,VerSiErrorAlPublicar);
+});
+function VerSiErrorAlPublicar(respuesta){
+    if(respuesta === "error"){
+        alert("Hubo un error");
+    }else{
+        ActualizarMisProductos();
+        
+    }
+    
+}
+
+function ActualizarMisProductos(){
+    $.post("/Vendiendo",credencial,InyectarContenidoMisProductos);
+
+}
+function InyectarContenidoMisProductos(respuesta){
+    cardColumnsMisProductos.innerHTML = respuesta;
+}
