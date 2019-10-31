caccf66273f9d1fa3f15840650a0b3c083986cdf
erik <erik10cavazos@gmail.com>
2019-10-31 04:01:33 -0600

    obtener producto de acuerdo a filtro especifico (terminar, solo devuelve recientes y todos). Formulario de registro de producto se vacia y oculta al publicar producto

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -28,6 +28,8 @@ $('#campo_pass_comprobacion').on('keyup',function () {
 $('#boton_publicar_producto_nuevo').on('click',function () {
     var DatosAEnviar = $('#formulario_publicar_producto').serialize();
     $.post("/Vender",DatosAEnviar,VerSiErrorAlPublicar);
+    document.getElementById("formulario_publicar_producto").reset();
+    $('#form').collapse('hide');
 });
 function VerSiErrorAlPublicar(respuesta){
     if(respuesta === "error"){
@@ -68,8 +70,37 @@ function obtenerProductosRecienPublicados(){
 function inyectarContenidoProductosRecientes(resultado){
     acaboDePublicar_Div.innerHTML = resultado;
 }
-function obtenerProductosPublicosRecientes(idUsuario){
-    $.get("/ObtenerProductosPublicos",{recientes:"true", usuarioAExcluir:idUsuario},function(respuesta){
-        productosRecientes_Div.innerHTML = respuesta;
-    })
+function obtenerProductosPublicosRecientes(idUsuario,desde){
+    //parametro "desde":
+    //1=>Principal
+    //2=>MostrarTodo
+    //Agregar mas aqui
+    switch(desde){
+        case 1: $.get("/ObtenerProductosPublicos",{recientes:"true", usuarioAExcluir:idUsuario},function(respuesta){
+            productosRecientes_Div.innerHTML = respuesta;
+        });break;
+        case 2: $.get("/ObtenerProductosPublicos",{usuarioAExcluir:idUsuario},function(respuesta){
+            pantallaVerTodo.innerHTML = respuesta;
+        });break;
+    }
+    
+}
+function obtenerInfoProducto(idProducto,desde){
+    //parametro "desde":
+    //1=>Principal
+    //2=>MostrarTodo
+    //Agregar mas aqui
+    switch (desde) {
+        case 1: $.get("/MostrarInfoProducto",{idProducto:idProducto,DesdePrincipal:"true"},function(respuesta){
+            pantallaInfoProducto.innerHTML = respuesta;
+        });break;
+        case 2: $.get("/MostrarInfoProducto",{idProducto:idProducto,DesdeVerTodo:"true"},function(respuesta){
+            pantallaInfoProducto.innerHTML = respuesta;
+        });break;
+        default: $.get("/MostrarInfoProducto",{idProducto:idProducto},function(respuesta){
+            pantallaInfoProducto.innerHTML = respuesta;
+        });break;
+    }
+    
+    mostrarInfoProducto();
 }
