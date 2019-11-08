24f9e75d251bb527e92e97409acfbcdf4aa9466d
erik <erik10cavazos@gmail.com>
2019-11-08 11:14:54 -0600

    regreso id de producto registrado para su uso en "actualizarinfo"

--- Pages/Vender.cshtml.cs
+++ Pages/Vender.cshtml.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Data.Common;
+using System.IO;
 using mercado_uanl.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -7,21 +11,24 @@ namespace mercado_uanl.Pages
     public class Vender : PageModel
     {
         private readonly DbContextApp contexto;
+        public int Id;
 
         public Vender(DbContextApp contexto)
         {
             this.contexto = contexto;
         }
 
-        public bool ExistioError = false;
-        public IActionResult OnPost(string id,string pass,string Nombre,string Categoria, string precio, string urlImagen="",string Descripcion="")
+        public bool ExistioError;
+        public IActionResult OnPost(string id,string pass,string Nombre,string Categoria, string precio, string Descripcion="")
         {
+            
+            
+            
             var ProductoNuevo = new Producto();
             ProductoNuevo.Nombre = Nombre;
             ProductoNuevo.Descripcion = Descripcion;
             ProductoNuevo.Categoria = Categoria;
             ProductoNuevo.Precio = float.Parse(precio);
-            ProductoNuevo.UrlFoto = urlImagen;
             ProductoNuevo.IdUsuario = int.Parse(id);
             ProductoNuevo.Publico = true;
 
@@ -29,12 +36,15 @@ namespace mercado_uanl.Pages
             {
                 contexto.Productos.Add(ProductoNuevo);
                 contexto.SaveChanges();
+                contexto.Entry(ProductoNuevo).GetDatabaseValues();
+                Id = ProductoNuevo.Id;
+                ExistioError = false;
             }
             else
             {
                 ExistioError = true;
             }
-            
+
             return Page();
         }
     }
