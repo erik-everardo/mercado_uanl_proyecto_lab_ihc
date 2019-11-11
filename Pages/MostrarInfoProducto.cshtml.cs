181f035c02f8985457e994eaf3621008464fc73b
erik <erik10cavazos@gmail.com>
2019-11-11 02:08:46 -0600

    agregados campos desde (queda pendiente categoria y calificaion)

--- Pages/MostrarInfoProducto.cshtml.cs
+++ Pages/MostrarInfoProducto.cshtml.cs
@@ -7,16 +7,36 @@ namespace mercado_uanl.Pages
     {
         private readonly DbContextApp contexto;
         public Producto producto;
-        public bool DesdePrincipal, DesdeVerTodo;
+        public bool DesdePrincipal, DesdeVerTodo, DesdeBusqueda, DesdeCU,DesdeMederos,DesdeSalud,DesdeLinares,DesdeSH,DesdeAgro;
 
         public MostrarInfoProducto(DbContextApp contexto)
         {
             this.contexto = contexto;
         }
-        public void OnGet(string idProducto, string DesdePrincipal="false", string DesdeVerTodo="false")
+        //veo de que pantalla vengo para saber a donde debe mandar boton azul (el que tiene la flecha)
+        //agregar mas variables: desde categorias y calificacion
+        public void OnGet(string idProducto, 
+            string DesdePrincipal="false", 
+            string DesdeVerTodo="false", 
+            string DesdeBusqueda="false",
+            string DesdeCU = "false",
+            string DesdeMederos ="false",
+            string DesdeSalud = "false",
+            string DesdeLinares = "false",
+            string DesdeSH = "false",
+            string DesdeAgro = "false"
+            )
         {
             this.DesdePrincipal = DesdePrincipal == "true";
             this.DesdeVerTodo = DesdeVerTodo == "true";
+            this.DesdeBusqueda = DesdeBusqueda == "true";
+            this.DesdeCU = DesdeCU == "true";
+            this.DesdeMederos = DesdeMederos == "true";
+            this.DesdeSalud = DesdeSalud == "true";
+            this.DesdeLinares = DesdeLinares == "true";
+            this.DesdeSH = DesdeSH == "true";
+            this.DesdeAgro = DesdeAgro == "true";
+            
             producto = contexto.Productos.Find(int.Parse(idProducto));
         }
 
