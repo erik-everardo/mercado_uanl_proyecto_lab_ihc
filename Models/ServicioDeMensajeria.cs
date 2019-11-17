eed50a68635f6730c5a0f14ab9113acacdda349d
erik <erik10cavazos@gmail.com>
2019-11-17 09:39:10 -0600

    agrego "Dato" y corrigo getters y setters

--- Models/ServicioDeMensajeria.cs
+++ Models/ServicioDeMensajeria.cs
@@ -2,11 +2,12 @@ namespace mercado_uanl.Models
 {
     public class ServicioDeMensajeria
     {
-        public int Id;
-        public int IdUsuario;
+        public int Id { get; set; }
+        public int IdUsuario { get; set; }
         //1:WhatsApp
         //2:Facebook Messenger
         //3:Telegram
-        public int Servicio;
+        public int Servicio { get; set; }
+        public string Dato { get; set; }
     }
 }
